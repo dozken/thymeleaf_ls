a8f49385bb22d469e2462eff0f9801344deec261
dozken <dozken@users.noreply.github.com>
2023-12-25 23:50:13 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Thymeleaf Language Server Protocol (LSP)
+
+## Note
+⚠️ **Work in Progress:** This project is currently under development and is in the early stages. Contributions and feedback are highly appreciated as we work towards improving and expanding the Thymeleaf Language Server Protocol.
+
+## Overview
+
+This is a Language Server Protocol (LSP) implementation for Thymeleaf, a modern server-side Java template engine for web and standalone environments. The Thymeleaf LSP aims to provide enhanced development support for Thymeleaf templates in various integrated development environments (IDEs) and text editors.
+
+## Features
+
+- [ ] **Code Completion:** Get intelligent code completion suggestions for Thymeleaf attributes and expressions as you type.  
+- [ ] **Syntax Highlighting:** Enjoy syntax highlighting for Thymeleaf expressions and tags in your IDE or text editor.
+- [ ] **Navigation:** Easily navigate through Thymeleaf templates with support for jumping to declarations and references.
+- [ ] **Error Checking:** Receive real-time feedback on syntax errors and other issues in your Thymeleaf templates.
+- [ ] **Documentation:** Access documentation and tooltips for Thymeleaf attributes and expressions right in your editor.
+
---
+++ src/main.rs
@@ -0,0 +1,158 @@
+use std::ops::Deref;
+use std::path::Path;
+
+// use completion::get_completions;
+// use references::references;
+use tokio::sync::RwLock;
+
+// use gotodef::goto_definition;
+use tower_lsp::jsonrpc::{Error, ErrorCode, Result};
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+// use vault::{Vault, construct_vault, reconstruct_vault};
+
+// mod vault;
+// mod gotodef;
+// mod references;
+// mod completion;
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+    // vault: RwLock<Option<Vault>>
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, i: InitializeParams) -> Result<InitializeResult> {
+        let Some(root_uri) = i.root_uri else {
+            return Err(Error::new(ErrorCode::InvalidParams));
+        };
+        let root_dir = Path::new(root_uri.path());
+
+        return Ok(InitializeResult {
+            server_info: None,
+            capabilities: ServerCapabilities {
+                text_document_sync: Some(TextDocumentSyncCapability::Kind(
+                    TextDocumentSyncKind::FULL,
+                )),
+                completion_provider: Some(CompletionOptions {
+                    resolve_provider: Some(false),
+                    trigger_characters: Some(vec!["#".to_string(), "[".into()]),
+                    work_done_progress_options: Default::default(),
+                    all_commit_characters: None,
+                    completion_item: None,
+                }),
+                // definition: Some(GotoCapability::default()),
+                definition_provider: Some(OneOf::Left(true)),
+                references_provider: Some(OneOf::Left(true)),
+                rename_provider: Some(OneOf::Left(true)),
+                ..Default::default()
+            },
+        });
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client
+            .log_message(MessageType::INFO, "Obsidian_ls initialized")
+            .await;
+    }
+
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        // let Some(ref mut vault) = *self.vault.write().await else {
+        //     self.client.log_message(MessageType::ERROR, "Vault is not initialized").await;
+        //     return;
+        // };
+        //
+        // let Ok(path) = params.text_document.uri.to_file_path() else {
+        //     self.client.log_message(MessageType::ERROR, "Failed to parse URI path").await;
+        //     return;
+        // };
+        // let text = params.text_document.text;
+        // reconstruct_vault(vault, (&path, &text));
+    }
+
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        // let Some(ref mut vault) = *self.vault.write().await else {
+        //     self.client.log_message(MessageType::ERROR, "Vault is not initialized").await;
+        //     return;
+        // };
+        //
+        // let Ok(path) = params.text_document.uri.to_file_path() else {
+        //     self.client.log_message(MessageType::ERROR, "Failed to parse URI path").await;
+        //     return;
+        // };
+        // let text = &params.content_changes[0].text;
+        // reconstruct_vault(vault, (&path, text));
+    }
+
+    // async fn goto_definition(
+    //     &self,
+    //     params: GotoDefinitionParams,
+    // ) -> Result<Option<GotoDefinitionResponse>> {
+    //
+    //     let position = params.text_document_position_params.position;
+    //
+    //     let vault_option = self.vault.read().await;
+    //     let Some(vault) = vault_option.deref() else {
+    //         return Err(Error::new(ErrorCode::ServerError(0)));
+    //     };
+    //     let Ok(path) = params.text_document_position_params.text_document.uri.to_file_path() else {
+    //         return Err(Error::new(ErrorCode::ServerError(0)));
+    //     };
+    //     let result = goto_definition(&vault, position, &path);
+    //
+    //
+    //     return Ok(result.map(|l| GotoDefinitionResponse::Array(l)))
+    // }
+
+    // async fn references(&self, params: ReferenceParams) -> Result<Option<Vec<Location>>> {
+    //     let position = params.text_document_position.position;
+    //
+    //     let vault_option = self.vault.read().await;
+    //     let Some(vault) = vault_option.deref() else {
+    //         return Err(Error::new(ErrorCode::ServerError(0)));
+    //     };
+    //     let Ok(path) = params.text_document_position.text_document.uri.to_file_path() else {
+    //         return Err(Error::new(ErrorCode::ServerError(0)));
+    //     };
+    //
+    //     let locations = references(vault, position, &path);
+    //     Ok(locations)
+    // }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+    async fn completion(&self, params: CompletionParams) -> Result<Option<CompletionResponse>> {
+        // let bad_vault = self.vault.read().await;
+        // let Some(vault) = bad_vault.deref() else {
+        //     return Err(Error::new(ErrorCode::ServerError(0)))
+        // };
+        // let completions = get_completions(vault, &params);
+        // if completions == None {
+        //     self.client.log_message(MessageType::INFO, format!("No completions for: {:?}", params)).await;
+        // }
+        // Ok(completions)
+        //
+        //
+        // Ok(completions.map(CompletionResponse::Array))
+        let completions = CompletionResponse::Array(vec![
+            CompletionItem::new_simple("thyme".into(), "leaf".into()),
+            CompletionItem::new_simple("hello".into(), "world".into()),
+        ]);
+
+        println!("got here");
+        return Ok(Some(completions));
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let (service, socket) = LspService::new(|client| Backend { client });
+    Server::new(stdin, stdout, socket).serve(service).await;
+}
